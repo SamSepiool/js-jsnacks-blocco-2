f32c2f5b996adabd76f323a9110061606cae3be4
SamSepiool <simone.severini@outlook.it>
2021-08-27 11:46:10 +0200

    add 'for' version

---
+++ snack-1/script.js
@@ -0,0 +1,16 @@
+// chiedere per 5 volte all’utente di inserire un numero.
+var listaNumeri = [];
+var nuovoNumero = 0;
+for (var i = 0; i < 5; i++) {
+
+    var nuovoNumero = prompt('inserisci un numero');
+    listaNumeri.push(nuovoNumero);
+}
+// l programma stampa la somma di tutti i numeri inseriti.
+console.log(listaNumeri);
+
+for ( var i = 0; i < listaNumeri.length; i++) {
+    var li = document.createElement('li');
+    li.textContent = listaNumeri[i];
+    document.getElementById('numbers').appendChild(li);
+}
